290ff1632da7b6ee7f4476583f8b481116787cb7
CamysN <camyneves123@gmail.com>
2024-02-25 15:33:32 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,21 @@
+
+const key = "d9f6b622c4206d94c5bead9e5efc73c5"
+
+function colocarDadosNaTela(dados) {
+    document.querySelector("#temperatura").innerHTML = Math.floor(dados.main.temp) + "°C"
+    document.querySelector("#cidade").innerHTML = dados.name
+    document.querySelector("#umidade").innerHTML = "Umidade: " + dados.main.humidity + "%"
+    document.querySelector("#clima").innerHTML = dados.weather[0].description
+    document.querySelector("#icon").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
+    document.querySelector("#farenheit").innerHTML = Math.floor(dados.main.temp)*9 /5 +32 +"°F"
+}
+async function buscarCidade(cidade) {
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then( resposta => resposta.json())
+
+    colocarDadosNaTela(dados) 
+}
+function clicarNoBotão() {
+    const cidade = document.querySelector("input").value
+
+    buscarCidade(cidade)
+}
